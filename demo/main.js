2e5f6fabb16d1073cb57015d206d05a520fced16
Zeno Zeng <zenoofzeng@gmail.com>
2014-08-24 15:06:58 +0800

    update demo

--- demo/main.js
+++ demo/main.js
@@ -1,9 +1,28 @@
+var drawColors = function(colors) {
+    var div = colors.map(function(color) {
+        return "<div class='color' style='background: rgb(" + color.join(',') + ")'></div>";
+    });
+    $('#colors').html(div.join(''));
+};
+
+var drawColorSchemes = function(colorSchemes) {
+    // add new schemes
+    colorSchemes.forEach(function(scheme) {
+        var elem = $("[data-scheme='" + scheme.join() + "']");
+        if(elem[0]) return;
+        scheme = scheme.map(function(color) {
+            return "<div class='color' style='background: rgb(" + color.join(',') + ")'></div>";
+        });
+        $('#schemes').append("<div class='scheme'>" + scheme.join('') + "</div>");
+    });
+};
 
 $.get('pool.json', function(data) {
     var opts = {};
     opts.genes = data.map(function(data) {
         return data.rgb;
     });
+    drawColors(opts.genes);
     opts.weights = data.map(function(data) {
         return data.weight;
     });
@@ -19,7 +38,7 @@ $.get('pool.json', function(data) {
         return fitness;
     };
     var population = new GenePool(opts);
-    console.log(population.toArray());
+    drawColorSchemes(population.toArray());
     window.population = population;
     population.next();
 });
--- watch.sh
+++ watch.sh
@@ -2,6 +2,6 @@
 # sudo apt-get install inotify-tools
 
 while true; do
-    inotifywait -r -e create,delete,modify,move .
+    inotifywait --exclude .git -r -e create,delete,modify,move .
     ./build.sh
 done
